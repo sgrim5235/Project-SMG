94cb408ca2c68430823db4b99c5c65e058ef0be3
sgrim5235 <sgrim5235@ung.edu>
2016-11-09 15:32:06 -0500

    Added arcpy imports

---
+++ Untitled.py
@@ -0,0 +1,4 @@
+import arcpy #imports arcpy package
+import arcpy.sa #imports spatial analysis module
+import arcpy.ga #imports geostatistical analyst module
+import arcpy.mapping #imports mapping module
