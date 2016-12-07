fe84649d01a0ada588a7eee28c8cef9780f0b699
Sarah Grimsley (SGRIM5235) <SGRIM5235@ung.edu>
2016-12-06 22:29:50 -0500

    Changed file name. Changed file name and added code

---
+++ SMG_Project.py
@@ -0,0 +1,8 @@
+import arcpy
+
+CountDict={}
+def dups(x):
+    x={}
+    for i in x:
+        x[item]=item.count()
+    return x
--- Untitled.py
+++
@@ -1,4 +0,0 @@
-import arcpy #imports arcpy package
-import arcpy.sa #imports spatial analysis module
-import arcpy.ga #imports geostatistical analyst module
-import arcpy.mapping #imports mapping module
