3756765603d9ef53aa655e7461bf6ca23d610eb9
Sarah Grimsley (SGRIM5235) <SGRIM5235@ung.edu>
2016-12-10 20:23:15 -0500

    Rough finish. Finished using python script in arcmap, needs to be neater.

--- SMG_Project.py
+++ SMG_Project.py
@@ -1,4 +1,7 @@
 import arcpy
+import os
+import sys
+
 
 #Added Extra Field to 2006 Attribute Table to Calculate Duplicate Fires
 #Named Field "DUPS"
@@ -9,15 +12,33 @@ def dups(x):
     dup_dict[x]=dup_dict.setdefault(x,-1)+1
     return dup_dict[x]
 
-#Enter into Python Window to find days in 2006 that had thirty or more fires
+#Enter into Python Window to find days in 2006 that had fifty or more fires
+#This code saves me from individually going through every single date
+#In every single year and building the query to show me individual repeated dates
+#And physically counting all the duplicates for all ten years
 #Using this data to find hotspots
-cursor_location=arcpy.da.SearchCursor("2006","DUPS")
-test=(29.0,) #Tests for days that had thirty or more fires, in tuple format because
-#that is how the rows are printed for some reason...haven't figured that out
+#Set for days with fifty+ fires
+
+cursor_location=arcpy.da.SearchCursor("2006",["DUPS","JDATE"]) #Change year accordingly
+test=(49.0, 2006005) #Tests for days that had fifty or more fire and shows the Julian date as an identifier
+fifty=[] #Empty list to put for loop results into
+#Must find first Julian Date from attribute table to use here in order for it to work
 for row in cursor_location:
     if row>test:
-        print row
+        fifty.append(row)
 
-#Need to find a way to print row identifier as well, like the Julian Date
-#Will have to do same test for every year
-#Also need to add the DUPS field to 2006-2016
+#Make function that gets rid of the days that are printed multiple times
+        #Due to having more than one number over 49
+
+def rid_dupes(x):
+    uniques=set()
+    for i in x:
+        if not uniques.intersection(i):
+            yield i
+            uniques.update(i)
+
+
+#List the days with 50+ fires to use to run hot spot tool
+list(rid_dupes(fifty))
+
+#Will have to do same tests for every year
